0dc761e5d3fea42b6c44a050f7da1f7917c7dbe2
Simon Nielsen <simon@minatjanster.se>
2019-03-04 14:57:11 +0100

    Added tp link cloud auth

--- tangible-mat/tangible-mat.ino
+++ tangible-mat/tangible-mat.ino
@@ -1,3 +1,7 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
 int irAnalogPin = A0;
 int restAreaButtonPin = 0;
 int coffeeButtonPin = 1;
@@ -38,12 +42,25 @@ ProcessedInput processedInput = {
 
 unsigned long musicButtonStartHold = 0;
 
+int tpNotConnected = 0;
+int tpConnecting = 1;
+int tpConnected = 2;
+int tpFailedToConnect = 3;
+int tpLinkCloudConnection = notConnected;
+String tpLinkToken = "";
+
 void setup() {
   pinMode(coffeeButtonPin, INPUT);
   pinMode(musicButtonPin, INPUT);
+
+  Serial.begin(9600);
+  connectToWifi();
 }
 
 void loop() {
+  if(WiFi.status() == WL_CONNECTED && tpLinkCloudConnection == tpNotConnected){
+    loginToIotLampCloud();
+  }
   readMatInput();
   processMatInput();
   if(processedInput.musicButtonPressed) {
@@ -88,4 +105,32 @@ void processMatInput() {
   }
 
   lastCycleRawInput = currentRawInput;
+}
+
+void connectToWifi() {
+  WiFi.begin("tangi19", "Tangible19iot");   //WiFi connection
+}
+
+void loginToIotLampCloud() {
+  Serial.println("Logging in to tp link cloud");
+  tpLinkCloudConnection = tpConnecting;
+  StaticJsonBuffer<200> jsonBuffer;
+  
+  HTTPClient http;
+  http.begin("https://wap.tplinkcloud.com");
+  http.addHeader("Content-Type", "application/json");
+  int httpCode = http.POST("{\"method\": \"login\",\"params\": {\"appType\": \"Kasa_Android\",\"cloudUserName\": \"stina.vk@gmail.com\",\"cloudPassword\": \"Tangible\",\"terminalUUID\": \"015ea6b4-26f4-4d50-af6f-d543024f57b8\"}}");
+  Serial.println("Login post request http code: " + httpCode);
+  String payload = http.getString();
+  http.end();
+  JsonObject& root = jsonBuffer.parseObject(payload);
+  if (!root.success()) {
+    Serial.println("parseObject() failed");
+    tpLinkCloudConnection = tpFailedToConnect;
+    return;
+  } else {
+    tpLinkToken = root["token"];
+    tpLinkCloudConnection = tpConnected;
+    Serial.println("Got token and connected to the tp link cloud");
+  }
 }
